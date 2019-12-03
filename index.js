3ab6437a17b936f35806cdff0225a0854b6db5e0
bix <840573+bix6@users.noreply.github.com>
2019-12-03 09:38:49 -0800

    shorten code for formSubmitted and validateUserInput

--- index.js
+++ index.js
@@ -3,23 +3,16 @@
 // Ensure user selects at least one required checkbox
 // Thank you: https://stackoverflow.com/questions/6218494/using-the-html5-required-attribute-for-a-group-of-checkboxes
 function validateUserInput() {
-    // TODO delete console log
-    console.log($(".required-cb :checkbox:checked").length);
-
     return ($(".required-cb :checkbox:checked").length > 0);
 }
 
+// Handle User Form Submission
 function formSubmitted() {
     $(".user-input-form").on("submit", function(event) {
         event.preventDefault();
 
-        const validInput = validateUserInput();
-
-        if (validInput) {
-            console.log('Good Input');
-        }
-        else {
-            console.log('Bad Input');
+        if (!validateUserInput()) {
+            alert('Please select a valid surf spot.');
         }
     })
 }
